a93d685827146108e63b85608ebec18d1c968523
eora21 <eora21@naver.com>
2023-06-07 13:00:33 +0900

    feat: 🎸 추가 ID/PW 및 OAuth 로그인에 대해 모두 적용할 수 있는 유저 객체 작성

---
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/auth/model/GatewayUser.java
@@ -0,0 +1,61 @@
+package com.nhnacademy.mini_dooray.gateway.auth.model;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class GatewayUser implements UserDetails, OAuth2User {
+    private final String username;
+    private final String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    public String getName() {
+        return getUsername();
+    }
+}
