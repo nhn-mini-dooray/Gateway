d2a172a42a0b45cd8d6251dc666320f4e2d80e6c
eora21 <eora21@naver.com>
2023-06-07 17:07:05 +0900

    fix: 🐛 변경 redis 정상적인 연결을 위해 필드, setter 및 역직렬화 어노테이션 작성
    해당 필드 및 세팅들이 존재하지 않으면 redis에 넣어줬던 값들을 다시 받아올 수 없어 계속해서 exception이 터졌다.

--- src/main/java/com/nhnacademy/mini_dooray/gateway/auth/model/GatewayUser.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/auth/model/GatewayUser.java
@@ -1,57 +1,41 @@
 package com.nhnacademy.mini_dooray.gateway.auth.model;
 
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
 
-@RequiredArgsConstructor
+@Getter
+@Setter
+@JsonDeserialize
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GatewayUser implements UserDetails, OAuth2User {
-    private final String username;
-    private final String password;
+    private String username;
+    private String password;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+    private List<GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-    @Override
-    public Map<String, Object> getAttributes() {
-        return Collections.emptyMap();
+    public GatewayUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+        accountNonExpired = true;
+        accountNonLocked = true;
+        credentialsNonExpired = true;
+        enabled = true;
+        authorities = new ArrayList<>();
+        attributes = new HashMap<>();
     }
 
     @Override
