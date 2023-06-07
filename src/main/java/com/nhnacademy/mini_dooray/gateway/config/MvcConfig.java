b649eb7ba449bae77758a1c9c5edad02ec8ef9e5
eora21 <eora21@naver.com>
2023-06-07 13:04:53 +0900

    feat: 🎸 추가 api 연결을 위한 RestTemplate Bean 등록

--- src/main/java/com/nhnacademy/mini_dooray/gateway/config/MvcConfig.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/config/MvcConfig.java
@@ -1,13 +1,27 @@
 package com.nhnacademy.mini_dooray.gateway.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.Duration;
+
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
+    private static final long TIME_OUT_SECONDS = 3L;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setReadTimeout(Duration.ofSeconds(TIME_OUT_SECONDS))
+                .setConnectTimeout(Duration.ofSeconds(TIME_OUT_SECONDS))
+                .build();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
