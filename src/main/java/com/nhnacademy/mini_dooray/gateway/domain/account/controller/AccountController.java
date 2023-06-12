f7c82f70a01c5c8772ddedd2552d4ec4db88cdf4
eora21 <eora21@naver.com>
2023-06-12 20:21:56 +0900

    refactor: 💡 변경 dto를 사용하여 회원가입을 요청하도록 수정

--- src/main/java/com/nhnacademy/mini_dooray/gateway/domain/account/controller/AccountController.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/domain/account/controller/AccountController.java
@@ -1,11 +1,10 @@
 package com.nhnacademy.mini_dooray.gateway.domain.account.controller;
 
 import com.nhnacademy.mini_dooray.gateway.auth.service.ApiUserDetailsService;
-import com.nhnacademy.mini_dooray.gateway.domain.account.model.Account;
+import com.nhnacademy.mini_dooray.gateway.domain.account.model.request.SignUpRequestDto;
 import com.nhnacademy.mini_dooray.gateway.domain.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountController {
     private final AccountService accountService;
-    private final PasswordEncoder passwordEncoder;
     private final ApiUserDetailsService apiUserDetailsService;
 
     @PostMapping("signUp")
-    public String signUp(Account account) {
-        String encodePassword = passwordEncoder.encode(account.getPassword());
-        account.setPassword(encodePassword);
-        accountService.signUp(account);
-        UserDetails userDetails = apiUserDetailsService.loadUserByUsername(account.getLoginId());
+    public String signUp(SignUpRequestDto signUpRequestDto) {
+        accountService.signUp(signUpRequestDto);
+        UserDetails userDetails = apiUserDetailsService.loadUserByUsername(signUpRequestDto.getLoginId());
         apiUserDetailsService.forceLogin(userDetails);
-        return "redirect:/";
+        return "redirect:/projects";
     }
 }
