83121c8849a6f4426dd7a7a43149f4e8e564ef4a
eora21 <eora21@naver.com>
2023-06-12 20:19:52 +0900

    feat: 🎸 추가 회원가입 api 요청하는 서비스 작성

--- src/main/java/com/nhnacademy/mini_dooray/gateway/domain/account/service/AccountService.java
+++ src/main/java/com/nhnacademy/mini_dooray/gateway/domain/account/service/AccountService.java
@@ -1,15 +1,30 @@
 package com.nhnacademy.mini_dooray.gateway.domain.account.service;
 
-import com.nhnacademy.mini_dooray.gateway.domain.account.model.Account;
-import com.nhnacademy.mini_dooray.gateway.domain.account.repository.MemoryRepository;
+import com.nhnacademy.mini_dooray.gateway.domain.account.model.request.SignUpRequestDto;
+import com.nhnacademy.mini_dooray.gateway.properties.AccountProperties;
+import com.nhnacademy.mini_dooray.gateway.util.RequestApiHelper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
-    private final MemoryRepository memoryRepository;
-    public void signUp(Account account) {
-        memoryRepository.signUp(account);
+    private final PasswordEncoder passwordEncoder;
+    private final RequestApiHelper requestApiHelper;
+    private final AccountProperties accountProperties;
+
+    public void signUp(SignUpRequestDto signUpRequestDto) {
+        String encodePassword = passwordEncoder.encode(signUpRequestDto.getPassword());
+        signUpRequestDto.setPassword(encodePassword);
+
+        requestApiHelper.sendRequest(
+                accountProperties.getOrigin() + accountProperties.getSignup(),
+                HttpMethod.POST,
+                signUpRequestDto,
+                new ParameterizedTypeReference<>() {
+                });
     }
 }
